a890119460e780ceaaba41899f0464eafa9c0d74
karim melhem <melhem.karim@outlook.com>
2022-09-21 10:24:24 +0300

    updates

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Container,Row,Col } from 'react-bootstrap';
 import images from '../../constants/images';
 import './Header.css';
 
 const Header = () => (
-  <div className='app__header app__wrapper section__padding' id='home'>
+    <Row>
+      <Col sm>
+      <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app__wrapper_info'>
       <h1>Find Your Style </h1>
       <h1 className='app__header-h1'>The Key To Great Fashion</h1>
       <button type='button' className='custom__button button__hover'>Explore </button>
     </div>
-    <div className='app__wrapper_img'>
+  </div>
+
+      </Col>
+      <Col sm>
+      <div className='app__wrapper_img'>
         <img src={images.LL} alt='header-img'/>
       </div>
-  </div>
+      </Col>
+    </Row>
+ 
+    
 );
 
 export default Header;
